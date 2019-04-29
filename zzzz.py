82d2375f1cadbf7239abcaa722f4613c727f7901
larkwenhua <larkwenhua@163.com>
2019-04-29 15:09:30 +0800

    天眼查、pixiv

--- zzzz.py
+++ zzzz.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
-import hsdata
-
-# 获取卡组数据
-decks = hsdata.HSBoxDeck()
-# 若未找到本地数据，会自动从网络获取
-# print('从炉石盒子获取到', len(decks), '个卡组数据！')
-
-
-print(decks.wins)
-
+from tianyancha import Tianyancha
+# 单个
+table_dict = Tianyancha(username='18242260608', password='1475433947lark').tianyancha_scraper(keyword='安徽经邦', table=['baseInfo', 'staff', 'invest'], export='xlsx')
+# 批量
+# tuple_dicts = Tianyancha(username='User', password='Password').tianyancha_scraper_batch(input_template='input.xlsx', export='xlsx')
+# tuple_dicts[0]
